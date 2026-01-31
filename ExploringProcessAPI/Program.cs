a6cb6e531fbf957a6f379b076137e3e968e9cb2e
marianekk578 <marianekk578@gmail.com>
2026-01-31 20:14:31 +0100

    Repo init

---
+++ ExploringProcessAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+var sourceFile = "C:\\process\\NumberGenerator.cs";
+
+ProcessStartInfo startInfo = new ProcessStartInfo
+{
+    FileName = "dotnet",
+    Arguments = $"run \"{sourceFile}\"",
+    RedirectStandardOutput = true,
+    RedirectStandardError = true,
+    UseShellExecute = false
+};
+
+Process process = Process.Start(startInfo);
+
+process.OutputDataReceived += (sender, e) =>
+{
+    if (!string.IsNullOrEmpty(e.Data))
+    {
+        Console.Write($"{e.Data}");
+    }
+};
+
+process.ErrorDataReceived += (sender, e) =>
+{
+    if (!string.IsNullOrEmpty(e.Data))
+    {
+        Console.WriteLine($"Error: {e.Data}");
+    }
+};
+
+process.Start();
+process.BeginOutputReadLine();
+process.BeginErrorReadLine();
+Console.WriteLine($"Started process id = {process.Id}");
+await process.WaitForExitAsync();
